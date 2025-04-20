35062249382ad75c7ab5ba1aa316ab50e429b7fb
Newrgun Khompodoev <xompodoev@gmail.com>
2025-04-20 20:59:56 +0900

    3 iteration

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,41 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	Logger *log.Logger
+	http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+
+	router := chi.NewRouter()
+	router.Get("/", handlers.RootHandler)
+	router.Post("/upload", handlers.UploadHandler)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+
+	// Настройка HTTP-сервера
+	srv := &Server{
+		Logger: logger,
+		Server: http.Server{
+			Addr:         ":8080",
+			Handler:      router,
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		},
+	}
+
+	return srv
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func TextAndMorseCodeConversion(s string) (string, error) {
+	if isMorseCode(s) {
+		return morse.ToText(s), nil
+	} else if isPlainText(s) {
+		return morse.ToMorse(s), nil
+	}
+
+	return s, errors.New("the string contains invalid characters")
+}
+
+// isMorseCode проверяет, является ли строка кодом Морзе.
+func isMorseCode(s string) bool {
+	s = strings.TrimSpace(s)
+	return !strings.ContainsAny(s, "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890!?") &&
+		strings.ContainsAny(s, ".-")
+}
+
+// isPlainText проверяет, является ли строка обычным текстом.
+func isPlainText(s string) bool {
+	s = strings.TrimSpace(s)
+	return !strings.ContainsAny(s, ".-") &&
+		strings.ContainsAny(s, "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890!?")
+}
